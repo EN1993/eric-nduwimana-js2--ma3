00983feedd30a078b7a6990eded742c6013d8013
EN1993 <74547465+EN1993@users.noreply.github.com>
2021-12-10 19:19:15 +0100

    add

---
+++ JS/login.js
@@ -0,0 +1,24 @@
+import displayMessage from "./components/displayMessage.js";
+import { baseUrl } from "./settings/api";
+
+const form = document.querySelector("form");
+const username = document.querySelector("#username");
+const password = document.querySelector("#password");
+const message = document.querySelector(".message-container");
+
+form.addEventListener("submit", submitForm);
+
+function submitForm(event) {
+    event.preventDefault();
+
+    message.innerHTML = "";
+
+    const usernameValue = username.value.trim();
+    const passwordValue = password.value.trim();
+
+    if (usernameValue.length === 0 || passwordValue.length === 0) {
+        return displayMessage("warning", "Invalid values", ".message-container");
+    }
+
+    doLogin(usernameValue, passwordValue);
+}
